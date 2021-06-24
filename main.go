5b37c953d19c0873f0a4d371c22392eb2bac205d
Jack954-pixel <56510826+Jack954-pixel@users.noreply.github.com>
2021-06-24 12:03:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"net/http"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
-	}, nil
+var entries = []string{}
+
+func init() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
-func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(handler)
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+	    //serve the resource
+	    fmt.Fprintf(w, "<table><tr><th>Timestamp</th><th>Action</th><th>Data</th></tr>")
+	    for i, _ := range entries {
+	    	fmt.Fprintf(w, "%s", entries[len(entries) - i - 1])
+	    }
+	    fmt.Fprintf(w, "</table>")
+	case "POST":
+	    //add entry
+	    body, err := ioutil.ReadAll(r.Body)
+	    if err != nil {
+	    	fmt.Fprintf(w, err.Error())
+		}
+		entry := strings.SplitN(string(body), "|", 3)
+		new_entry := fmt.Sprintf("<tr><td>%s</td><td>%s</td> <td>%s</td></tr>", entry[0], entry[1], entry[2])
+	    entries = append(entries, new_entry)
+	    if len(entries) > 100 {
+	    	entries = entries[1:]
+	    }
+	    fmt.Fprintf(w, "POST\n")
+	default:
+	    //do nothing
+	}
 }
