1dad4421d50fcd83b3cfa9b61d48b330480a53a2
Jack954-pixel <56510826+Jack954-pixel@users.noreply.github.com>
2021-06-24 12:31:37 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,42 +1,27 @@
 package main
 
 import (
-	"io/ioutil"
-	"fmt"
-	"strings"
-	"net/http"
+    "flag"
+    "fmt"
+    "log"
+    "net/http"
+
+    "github.com/apex/gateway"
+    "github.com/carlmjohnson/feed2json"
 )
 
-var entries = []string{}
+func main() {
+    port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
+    flag.Parse()
+    listener := gateway.ListenAndServe
+    portStr := "n/a"
+    if *port != -1 {
+        portStr = fmt.Sprintf(":%d", *port)
+        listener = http.ListenAndServe
+        http.Handle("/", http.FileServer(http.Dir("./public")))
+    }
 
-func init() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-	    //serve the resource
-	    fmt.Fprintf(w, "<table><tr><th>Timestamp</th><th>Action</th><th>Data</th></tr>")
-	    for i, _ := range entries {
-	    	fmt.Fprintf(w, "%s", entries[len(entries) - i - 1])
-	    }
-	    fmt.Fprintf(w, "</table>")
-	case "POST":
-	    //add entry
-	    body, err := ioutil.ReadAll(r.Body)
-	    if err != nil {
-	    	fmt.Fprintf(w, err.Error())
-		}
-		entry := strings.SplitN(string(body), "|", 3)
-		new_entry := fmt.Sprintf("<tr><td>%s</td><td>%s</td> <td>%s</td></tr>", entry[0], entry[1], entry[2])
-	    entries = append(entries, new_entry)
-	    if len(entries) > 100 {
-	    	entries = entries[1:]
-	    }
-	    fmt.Fprintf(w, "POST\n")
-	default:
-	    //do nothing
-	}
+    http.Handle("/api/feed", feed2json.Handler(
+        feed2json.StaticURLInjector("https://news.ycombinator.com/rss"), nil, nil, nil))
+    log.Fatal(listener(portStr, nil))
 }
